43d5934105055a7d503d28eb8186da8411bad7f7
CzechVadim <139116076+CzechVadim@users.noreply.github.com>
2025-04-28 02:23:35 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,29 +3,8 @@ $('.slider__rev').slick({
   centerMode: true,
   centerPadding: '60px',
   slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
+  slidesToScroll: 1
 });
-
-
 
 $(document).ready(function(){
   $('.slider').slick({
