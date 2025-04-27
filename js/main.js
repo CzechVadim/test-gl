490f65e0331ee41773825d31c393122ea11c89c9
CzechVadim <139116076+CzechVadim@users.noreply.github.com>
2025-04-27 11:26:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,48 +1,40 @@
-var rev = $('.rev_slider');
-rev.on('init', function(event, slick, currentSlide) {
-  var
-    cur = $(slick.$slides[slick.currentSlide]),
-    next = cur.next(),
-    prev = cur.prev();
-  prev.addClass('slick-sprev');
-  next.addClass('slick-snext');
-  cur.removeClass('slick-snext').removeClass('slick-sprev');
-  slick.$prev = prev;
-  slick.$next = next;
-}).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-  console.log('beforeChange');
-  var
-    cur = $(slick.$slides[nextSlide]);
-  console.log(slick.$prev, slick.$next);
-  slick.$prev.removeClass('slick-sprev');
-  slick.$next.removeClass('slick-snext');
-  next = cur.next(),
-    prev = cur.prev();
-  prev.prev();
-  prev.next();
-  prev.addClass('slick-sprev');
-  next.addClass('slick-snext');
-  slick.$prev = prev;
-  slick.$next = next;
-  cur.removeClass('slick-next').removeClass('slick-sprev');
-});
-
-rev.slick({
-  speed: 1000,
-  arrows: true,
-  dots: false,
-  focusOnSelect: true,
-  prevArrow: '<button> prev</button>',
-  nextArrow: '<button> next</button>',
+$('.slider__rev').slick({
   infinite: true,
   centerMode: true,
-  slidesPerRow: 1,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  centerPadding: '0',
-  swipe: true,
-  customPaging: function(slider, i) {
-    return '';
-  },
-  /*infinite: false,*/
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: false,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    pauseOnFocus: true,
+    pauseOnНover: true
+  });
 });
